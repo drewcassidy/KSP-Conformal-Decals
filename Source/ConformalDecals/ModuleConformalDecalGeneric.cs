00908c9e1307fa320f17721c554636c3cac88ab2
drewcassidy <drewcassidy@me.com>
2020-06-02 22:45:48 -0700

    Get flag module working

--- Source/ConformalDecals/ModuleConformalDecalGeneric.cs
+++ Source/ConformalDecals/ModuleConformalDecalGeneric.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 namespace ConformalDecals {
     public class ModuleConformalDecalGeneric : ModuleConformalDecalBase {
         public override void OnLoad(ConfigNode node) {
-            base.OnLoad(node);
 
+            if (materialProperties == null) {
+                // materialProperties is null, so make a new one
+                materialProperties = ScriptableObject.CreateInstance<MaterialPropertyCollection>();
+                materialProperties.Initialize();
+            }
+            else {
+                // materialProperties already exists, so make a copy
+                materialProperties = ScriptableObject.Instantiate(materialProperties);
+            }
+
+            // set shader
+            materialProperties.SetShader(decalShader);
             // add texture nodes
             foreach (var textureNode in node.GetNodes("TEXTURE")) {
                 materialProperties.AddProperty(new MaterialTextureProperty(textureNode));
@@ -21,6 +32,8 @@ namespace ConformalDecals {
             foreach (var colorNode in node.GetNodes("COLOR")) {
                 materialProperties.AddProperty(new MaterialColorProperty(colorNode));
             }
+
+            base.OnLoad(node);
         }
     }
 }

de2f42a09f1851cfad5775869a4fe80e0ad349cc
drewcassidy <drewcassidy@me.com>
2020-06-08 02:16:30 -0700

    Temporarily remove nonfunctional intersection code
    Will probably fix at a later date, but its not needed for the plugin to work

--- Source/ConformalDecals/ProjectionTarget.cs
+++ Source/ConformalDecals/ProjectionTarget.cs
@@ -22,36 +22,30 @@ namespace ConformalDecals {
         }
 
         public void Project(Matrix4x4 orthoMatrix, OrientedBounds projectorBounds, Transform projector, bool useBaseNormal) {
-            var targetBounds = _targetRenderer.bounds;
-            if (projectorBounds.Intersects(targetBounds)) {
-                _projectionEnabled = true;
+            _projectionEnabled = true;
 
-                var targetMaterial = _targetRenderer.sharedMaterial;
-                var projectorToTargetMatrix = target.worldToLocalMatrix * projector.localToWorldMatrix;
+            var targetMaterial = _targetRenderer.sharedMaterial;
+            var projectorToTargetMatrix = target.worldToLocalMatrix * projector.localToWorldMatrix;
 
-                var projectionMatrix = orthoMatrix * projectorToTargetMatrix.inverse;
-                var decalNormal = projectorToTargetMatrix.MultiplyVector(Vector3.back).normalized;
-                var decalTangent = projectorToTargetMatrix.MultiplyVector(Vector3.right).normalized;
+            var projectionMatrix = orthoMatrix * projectorToTargetMatrix.inverse;
+            var decalNormal = projectorToTargetMatrix.MultiplyVector(Vector3.back).normalized;
+            var decalTangent = projectorToTargetMatrix.MultiplyVector(Vector3.right).normalized;
 
-                _decalMPB.SetMatrix(DecalPropertyIDs._ProjectionMatrix, projectionMatrix);
-                _decalMPB.SetVector(DecalPropertyIDs._DecalNormal, decalNormal);
-                _decalMPB.SetVector(DecalPropertyIDs._DecalTangent, decalTangent);
+            _decalMPB.SetMatrix(DecalPropertyIDs._ProjectionMatrix, projectionMatrix);
+            _decalMPB.SetVector(DecalPropertyIDs._DecalNormal, decalNormal);
+            _decalMPB.SetVector(DecalPropertyIDs._DecalTangent, decalTangent);
 
-                if (useBaseNormal && targetMaterial.HasProperty(DecalPropertyIDs._BumpMap)) {
-                    var normal = targetMaterial.GetTexture(DecalPropertyIDs._BumpMap);
-                    if (normal != null) {
+            if (useBaseNormal && targetMaterial.HasProperty(DecalPropertyIDs._BumpMap)) {
+                var normal = targetMaterial.GetTexture(DecalPropertyIDs._BumpMap);
+                if (normal != null) {
 
-                        _decalMPB.SetTexture(DecalPropertyIDs._BumpMap, targetMaterial.GetTexture(DecalPropertyIDs._BumpMap));
+                    _decalMPB.SetTexture(DecalPropertyIDs._BumpMap, targetMaterial.GetTexture(DecalPropertyIDs._BumpMap));
 
-                        var normalScale = targetMaterial.GetTextureScale(DecalPropertyIDs._BumpMap);
-                        var normalOffset = targetMaterial.GetTextureOffset(DecalPropertyIDs._BumpMap);
+                    var normalScale = targetMaterial.GetTextureScale(DecalPropertyIDs._BumpMap);
+                    var normalOffset = targetMaterial.GetTextureOffset(DecalPropertyIDs._BumpMap);
 
-                        _decalMPB.SetVector(DecalPropertyIDs._BumpMap_ST, new Vector4(normalScale.x, normalScale.y, normalOffset.x, normalOffset.y));
-                    }
+                    _decalMPB.SetVector(DecalPropertyIDs._BumpMap_ST, new Vector4(normalScale.x, normalScale.y, normalOffset.x, normalOffset.y));
                 }
-            }
-            else {
-                _projectionEnabled = false;
             }
         }
 
